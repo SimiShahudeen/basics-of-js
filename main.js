42ceae89c02a4a360fd2a5cdf65615323446f4b8
simi <alchemy.officialz@gmail.com>
2023-06-13 17:42:35 +0530

    learning progress (first commit)

---
+++ main.js
@@ -0,0 +1,32 @@
+let carList = ["volvo", "bmw", 'toyota', 'wagoner', 'hyundai'];
+let htmlElement = document.getElementById('list0');
+for (let brand of carList) {
+  let create = document.createElement('li');
+  create.innerText = brand
+  htmlElement.appendChild(create)
+  console.log(create);
+}
+
+let bikeList = ["Trek", "Specialized", "Giant", "Cannondale", "Scott"];
+let bikeListHtml = document.getElementById('list1');
+for (let bikeBrands of bikeList) {
+  let bikeListCreate = document.createElement('li');
+  bikeListCreate.innerText = bikeBrands
+  bikeListHtml.appendChild(bikeListCreate)
+}
+
+let pcList = ["Apple", "Dell", "HP", "Lenovo", "ASUS"];
+let pcHtml = document.getElementById('list2');
+for (let iteratePc of pcList) {
+  let createList = document.createElement('li');
+  createList.innerText = iteratePc
+  pcHtml.appendChild(createList)
+}
+
+let mobileArray = ["Apple", "Samsung", "Huawei", "Xiaomi", "OnePlus", "Google", "Sony", "LG", "Motorola", "Nokia"];
+let mobileHtml = document.getElementById('list3');
+for (let iterateMobile of mobileArray) {
+  let listCreating = document.createElement('li');
+  listCreating.innerText = iterateMobile
+  mobileHtml.appendChild(listCreating)
+}
